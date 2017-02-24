a67a6dcf56542de851b004a2c46cc7f5e36dd49a
katsukilei <katsukichama@gmail.com>
2017-02-24 15:54:22 -0500

    Update AdventureModel.java

--- src/adventure/AdventureModel.java
+++ src/adventure/AdventureModel.java
@@ -1,10 +1,10 @@
 /*
 This class:
-•	Creates the Rooms, the Things, and the Adventurer.
-•	Connects the Rooms with "paths" to other Rooms.
-•	Places Things in the Rooms.
-•	Places the Adventurer in some Room.
-•	Accepts commands from the player, and executes them.
+â€¢	Creates the Rooms, the Things, and the Adventurer.
+â€¢	Connects the Rooms with "paths" to other Rooms.
+â€¢	Places Things in the Rooms.
+â€¢	Places the Adventurer in some Room.
+â€¢	Accepts commands from the player, and executes them.
 	o	As commands are entered, they should be copied to the main text area.
 	o	The method that executes commands should return a String to be displayed in the main text area.
 */
@@ -13,4 +13,10 @@ package adventure;
 
 public class AdventureModel {
 
+	//A set of Action commands
+	protected static final String[] LOOK = new String[] {"look", "examine", "read"};
+	protected static final String[] TAKE = new String[] {"take", "get", "keep"};
+	protected static final String[] USE = new String[] {"use"};
+	protected static final String[] OPEN = new String[] {"open"};
+	protected static final String[] CLOSE = new String[] {"close"};
 }
