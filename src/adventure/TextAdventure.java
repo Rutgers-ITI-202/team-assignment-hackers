b15955c1460c18529225873b41cda3a6208f7627
katsukilei <katsukichama@gmail.com>
2017-02-24 15:20:52 -0500

    Update TextAdventure.java
    initial options, updated method call?

--- src/adventure/TextAdventure.java
+++ src/adventure/TextAdventure.java
@@ -1,39 +1,78 @@
 /*
 This class:
-•	Contains the main method used to start the game.
-•	Talks to the AdventureModel and to the Adventurer classes, as needed.
+â€¢	Contains the main method used to start the game.
+â€¢	Talks to the AdventureModel and to the Adventurer classes, as needed.
 */
 
 package adventure;
 import java.util.Scanner;
+/**
+ * This class will be the main method to start the game.
+ * @author Kajal, Kathy, Winnie
+ * @since 2.24.17
+ */
+
 
 public class TextAdventure {
 
-	private static Scanner sc;
+	Scanner sc;
+	private static String answer = "";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Adventurer Aron= new Aron();	
-		sc = new Scanner(System.in);
-		System.out.println("Welcome to Aron the Excapist. The plot will unravel as you play but first a tip." 
-				  + " Pick up any object you run into, you never know if you might need it. "
-				  + " Also, try to only visit the rooms once, and only pick things up once. GOOD LUCK!  \n\n");
-		String answer="";
-		System.out.println("Would you like to play? \n");
+		TextAdventure escape;
+		escape = new TextAdventure();
+		escape.Setup(); //makes a connection to setup
+		
+		//Aron Aron= new Aron() --fixed?
+	}
+		 
+	public void Setup(){	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("~~~ Welcome to Aron the Excapist. ~~~\n The plot will unravel as you play but first a tip.\n" 
+				  + " Pick up any object you run into, you never know if you might need it.\n"
+				  + " Also, try to only visit the rooms once, and only pick things up once."
+				  + " GOOD LUCK!  \n");
+		
+		System.out.println("Would you like to play?");
 		answer = sc.next();
 
-		if (answer.equalsIgnoreCase("No")){
-			System.out.println("Ok...");
+		if (answer.equalsIgnoreCase("no")){
+			System.out.println("Ok... Goodbye");
 			return;
-		} else if(answer.equalsIgnoreCase("Yes")){
+		} else if(answer.equalsIgnoreCase("yes")){
+				System.out.println("");
+				System.out.println("Aron is on his way home from a long day at work. He was looking forward\n"
+						+ "to spending time with his family. When he walks in his house he notices all the lights\n"
+						+ "lights are off. Papers all over the place, tables turned over, and no sign of Aron's family.\n"
+						+ " Aron calls out to his family but there is only dead silence.\n"
+						+ " Suddenly he feels a sharp pain in the back of his head and everything\n"
+						+ "goes black. Aron wakes up he in an unfamilar room with unconnected objects.\n"
+						+ "In front of Aron, he sees a table, a note on the wall, and a closed box on the floor.\n");}
 
-				System.out.println(" Aron is on his way home from a long day at work. He was looking forward "
-						+ "to spending time with his family. When he walks in his house he notices all the lights "
-						+ "lights are off. Evan is a code monkey and loves pushing buttons "
-						+ "are closed and the house was all out of order. Papers all over the place, tables "
-						+ "turned over, and no sign of Aron's family. Aron calls out to his family but there is "
-						+ "only dead silence. Suddenly he feels a sharp pain in the back of his head and everything "
-						+ "goes black. Aron wakes up he in an unfamilar room with unconnected objects. The note in "
-						+ " front of him reads 'Escape the room by connecting the clues or meet your fate'");
+ 		
+	System.out.println( "What will Aron do, player?\n");
+	System.out.println("Your choices are:\n"
+			+ "1. Go towards the table\n"
+			+ "2. Examine the note \n"
+			+ "3. Examine the box\n"
+			+ "4. Question why you are here\n");
+	//initial options
+			
+	int choice;
+	choice= sc.nextInt(); //reads payer's choice
+		if (choice==1){
+			System.out.println("The table seems to be made out of wood, and there is something taped in one of the table legs.");
+		}
+		else if (choice==2){
+			System.out.println("The note in front of him reads 'Escape the room by connecting the clues or to meet your fate.");
+		}
+		else if (choice==3){
+			System.out.println("The box is in a charcoal color with a combination lock attached to it.");
+		}
+		else if (choice==4){
+			System.out.println("Aron yells out: [Where in the world am I?].");
+		}
 	}
-
+	
 }
