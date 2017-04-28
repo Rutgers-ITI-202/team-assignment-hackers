9d9b32ac4fbb67a373c4c09444d2af2223d98e5f
katsukilei <katsukichama@gmail.com>
2017-04-28 17:58:34 -0400

    Add files via upload

--- src/adventure/Accessible.java
+++ src/adventure/Accessible.java
@@ -1,1 +1,20 @@
+/**
+ * 
+ * @author Kajal, Kathy, Winnie
+ * @version 1.0
+ * @since 1.0
+ * <h1> Puts the adventurer into the Starting room and initiates the game</h1>
+ * This interface implemented by class Door, Room which helps to make sure that the methods make the doors and 
+ * rooms work cooperate are there. 
+ */
 
+
+
+
+public interface Accessible {
+
+	//calls enter program from Room.java and sets locations to "this" which is defined in Aron.java
+	void enter(Adventurer p);
+	
+
+}
